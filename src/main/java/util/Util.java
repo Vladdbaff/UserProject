afd76dbd8240627d21db0bd06121be76f9d6b950
Vladdbaff <vladdbaf@mail.ru>
2021-08-02 14:35:23 +0300

    My message

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.sql.*;
+
+public class Util {
+    private final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final String LOGIN = "root";
+    private final String PASSWORD = "root";
+    private Connection connection;
+
+    public Util() {
+    }
+
+    public Connection getConnection() {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+
+        } catch (SQLException e) {
+            System.err.println("Соединение не установленно");
+        }
+        return connection;
+    }
+}
+
+
