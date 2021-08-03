25e9129e61ebab7358284eddef3c4fa09a8d45a1
Vladdbaff <vladdbaf@mail.ru>
2021-08-04 00:07:41 +0300

    Finish pls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,19 +13,19 @@ public class Main {
     public static void main(String[] args) {
 
         UserService us = new UserServiceImpl();
-        //us.createUsersTable();
+        us.createUsersTable();
 
-        /*us.saveUser("Саша", "Пупкин", (byte)31);
+        us.saveUser("Саша", "Пупкин", (byte)31);
         us.saveUser("Аня", "Ефимук", (byte) 20);
         us.saveUser("Егор", "Бандура", (byte)18);
-        us.saveUser("Влад", "Жданов", (byte)20);*/
+        us.saveUser("Влад", "Жданов", (byte)20);
 
 
         List<User> users = us.getAllUsers();
         System.out.println(users);
-        //us.cleanUsersTable();
+        us.cleanUsersTable();
 
-        //us.dropUsersTable();
+        us.dropUsersTable();
 
 
     }
